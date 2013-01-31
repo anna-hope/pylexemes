0e200bbc9bf4538c96aca8aa7a324138b12f05bf
Anton Osten <anton@ostensible.me>
2013-01-30 18:48:06 -0600

    - 0.3a

--- phonemeparser.py
+++ phonemeparser.py
@@ -3,24 +3,41 @@
 
 import json
 
-class PhonemeParser():
+class PhonemeParser:
 
-	def __init__(self):
+	def __init__(self, phonemes_f=None):
+
 		try:
-			phonemes = json.load(open("phonemes.json"))
+			if phonemes_f == None:
+				phonemes = json.load(open('phonemes.json'))
+			else:
+				phonemes = json.load(open(phonemes_f))
 		except Exception as e:
 			self.somethingwrong(e)
-		
+
+		self._phonemes = phonemes
 		self._symbols = []
+		self._names = []
 		self._features = []
 
 		try:
 			for n in phonemes:
 				self._symbols.append(n['symbol'])
+				self._names.append(n['name'])
 				self._features.append(list(n['features'].values()))
 		except Exception as e:
 			self.somethingwrong(e)
 
+	def phonemes():
+	    doc = "Phonemes as a dict."
+	    def fget(self):
+	        return self._phonemes
+	    def fset(self, value):
+	        self._phonemes = value
+	    def fdel(self):
+	        del self._phonemes
+	    return locals()
+	phonemes = property(**phonemes())
 	
 	def symbols():
 	    doc = "Phoneme symbols."
@@ -32,6 +49,17 @@ class PhonemeParser():
 	        del self._symbols
 	    return locals()
 	symbols = property(**symbols())
+
+	def names():
+	    doc = "Phoneme names."
+	    def fget(self):
+	        return self._names
+	    def fset(self, value):
+	        self._names = value
+	    def fdel(self):
+	        del self._names
+	    return locals()
+	names = property(**names())
 
 	def features():
 	    doc = "Phoneme features."
