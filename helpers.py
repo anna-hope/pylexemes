8cc17d90e871291bd0313563d30be6199fcef868
Anton Osten <anton@ostensible.me>
2013-05-18 22:56:43 -0500

    named tuples for feats, rewritten methods, and more

--- helpers.py
+++ helpers.py
@@ -3,6 +3,8 @@
 # http://ostensible.me
 
 import re
+from collections import namedtuple
+from difflib import SequenceMatcher
 try:
     import simplejson as json
 except ImportError:
@@ -18,6 +20,9 @@ class CustomError(Exception):
     
     def __str__(self):
         return self.message
+
+class SegmentParsingError(CustomError):
+    pass
 
 class FormParsingError(CustomError):
     pass
@@ -38,107 +43,37 @@ class SegmentParser:
         except Exception as e:
             self.somethingwrong(e)
 
-        self._segments = segments
-        self._symbols = []
-        self._names = {}
-        self._features = {}
+        self.segments = segments
+        self.symbols = []
+        self.names = {}
+        self.features = {}
+        
+        # a named tuple for storing the features of segments
+        Features = namedtuple('Features', sorted(segments[0]['features'].keys()))
 
         try:
-            for n in self._segments:
+            for n in self.segments:
                 symbol = n['symbol']
-                self._symbols.append(symbol)
-                self._names[symbol] = n['name']
-                self._features[symbol] = n['features']
+                self.symbols.append(symbol)
+                self.names[symbol] = n['name']
+                self.features[symbol] = Features(**n['features'])
 
         except Exception as e:
             self.somethingwrong(e)
+        
+        # this is useful for mapping features back to symbols
+        self.flipped_features = {self.features[segment]: segment for segment in self.features}
 
-        polysymbols = [n for n in self._symbols if len(n) > 1]
-        self._polysymbols = polysymbols
+        polysymbols = [n for n in self.symbols if len(n) > 1]
+        self.polysymbols = polysymbols
 
         true_features = {}
-        for s in self._features:
-             segment_true_features = [f for f in self._features[s] if self._features[s][f]]
-             true_features[s] = segment_true_features
-        self._true_features = true_features
-        self._duplicates = self.find_duplicates()
-
-    def segments():
-        doc = "Segments as a dictionary."
-        def fget(self):
-            return self._segments
-        def fset(self, value):
-            self._segments = value
-        def fdel(self):
-            del self._segments
-        return locals()
-    segments = property(**segments())
-    
-    def symbols():
-        doc = "Segment symbols as a lyst."
-        def fget(self):
-            return self._symbols
-        def fset(self, value):
-            self._symbols = value
-        def fdel(self):
-            del self._symbols
-        return locals()
-    symbols = property(**symbols())
-
-    def polysymbols():
-        doc = "Polysymbols."
-        def fget(self):
-            return self._polysymbols
-        def fset(self, value):
-            self._polysymbols = value
-        def fdel(self):
-            del self._polysymbols
-        return locals()
-    polysymbols = property(**polysymbols())
-
-    def names():
-        doc = "Segment names."
-        def fget(self):
-            return self._names
-        def fset(self, value):
-            self._names = value
-        def fdel(self):
-            del self._names
-        return locals()
-    names = property(**names())
-
-    def features():
-        doc = "All segment features (including false and non-applicable)."
-        def fget(self):
-            return self._features
-        def fset(self, value):
-            self._features = value
-        def fdel(self):
-            del self._features
-        return locals()
-    features = property(**features())
-
-    def true_features():
-        doc = "Only the active features of segments."
-        def fget(self):
-            return self._true_features
-        def fset(self, value):
-            self._true_features = value
-        def fdel(self):
-            del self._true_features
-        return locals()
-    true_features = property(**true_features())
-
-    def duplicates():
-        doc = "The duplicates property."
-        def fget(self):
-            return self._duplicates
-        def fset(self, value):
-            self._duplicates = value
-        def fdel(self):
-            del self._duplicates
-        return locals()
-    duplicates = property(**duplicates())
+        for s in self.features:
+            segment_features = vars(self.features[s])
+            segment_true_features = [f for f in segment_features if segment_features[f]]
+            true_features[s] = segment_true_features
+        self.true_features = true_features
+        self.duplicates = self.find_duplicates()
 
     def find_duplicates(self):
         doc = "Returns segments with the same features."
@@ -146,16 +81,18 @@ class SegmentParser:
         done_symbols = []
         done_features = []
         duplicate_groups = []
-        for s in self._features:
+        for s in self.features:
             # features of the current segment
-            cur_features = self._features[s]
+            cur_features = self.features[s]
             if cur_features in done_features:
                 for duplicate_group in duplicate_groups:
                     # if there is already more than a pair of segments with the same features
                     # we'll do this to add the current segment to the list of those duplicates
                     # first checking that we're not adding a duplicate (ha!) symbol
                     if done_symbols[done_features.index(cur_features)] in duplicate_group:
-                        symbol = [s for s in self._features if (s not in duplicate_group and self._features[s] == cur_features)][0]
+                        symbol = [s for s in self.features if (
+                                            s not in duplicate_group
+                                            and self.features[s] == cur_features)][0]
                         duplicate_group.append(symbol)
                         break
                 else:
@@ -163,23 +100,17 @@ class SegmentParser:
                     duplicate_groups.append([done_symbols[done_features.index(cur_features)], s])
             else:
                 done_symbols.append(s)
-                done_features.append(self._features[s])
+                done_features.append(self.features[s])
         return duplicate_groups
 
     def somethingwrong(self, e):
-        doc = "Invoked when something goes wrong with the segments.json file."
-        print(e)
-        quit('It seems that there is something wrong with the json file for segments. Check it over and run me again.')
+        '''Invoked when something goes wrong with the segments.json file.'''
+        raise SegmentParsingError(e)
         
 class FormParser:
     '''Parses forms to compute reconstructions from'''
 
-    def __init__(self, f=None):
-
-        if f != None:
-            lexemefile = f
-        else:
-            lexemefile = 'lexemes.json'
+    def __init__(self, file):
 
         try:
             self.lexemes = json.load(open(lexemefile))
@@ -244,7 +175,7 @@ class FormParser:
                     langs[lang_name.title()] = lang_code.casefold()
                 else:
                     # if there is a language with that code, but it's named differently, we could change the current instance of it
-                    # but I'm sure I want to do that
+                    # but I'm not sure I want to do that
                     pass
         if langs != json.load(open('langs.json')):
             json.dump(langs, open('langs.json', 'w'))
@@ -252,10 +183,8 @@ class FormParser:
     def somethingwrong(self, e):
         doc = "Invoked when there is something wron in the lexemes.json file."
         print("Error with %s" % e)
-        raise FormParsingError('''It seems that there is something wrong in the JSON file for lexemes. 
+        raise SegmentParsingError('''It seems that there is something wrong in the JSON file for lexemes. 
         Check it over and run me again.''')
-    
-
     
 class Form:
     '''A form'''
@@ -270,13 +199,13 @@ class Form:
                 # the string representation is the form itself
                 self.str = form
                 # tokenise it into segments
-                self.segments = self._tokenise(self.sp.polysymbols)
+                self.segments = self._tokenise()
                 # create a feature representation
-                self.features = self._to_features(self.sp.features)
+                self.features = self._to_features()
             # if it's a non-empty list
             elif form != []:
                 # its segments need to be converted into IPA symbols
-                self.segments = self._to_symbols(form, self.sp.features)
+                self.segments = self._to_symbols(form)
                 # its string representation are the joined segments
                 self.str = ''.join(self.segments)
             else:
@@ -287,6 +216,8 @@ class Form:
         elif isinstance(form, dict):
             if ['form'] in form and ['lang_code'] in form and ['gloss'] in form:
                 self.str = form['form']
+                self.segments = self._tokenise
+                self.features = self._to_features()
                 self.lang_code = form['lang_code']
                 self.gloss = form['gloss']
             else:
@@ -298,9 +229,20 @@ class Form:
         return self.str
     
     def __repr__(self):
-        return 'Form({})'.format(self.str)
+        return 'Form({form_as_str}, {lang_code}, {gloss})'.format(form_as_str = self.str,
+                                                              lang_code = self.lang_code,
+                                                              gloss = self.gloss)
     
-    def _tokenise(self, polysymbols):
+    def __eq__(self, form):
+        return (self.str == form.str 
+                and self.lang_code == form.lang_code
+                and self.gloss == form.gloss)
+                
+    def __hash__(self):
+        # the operators in the hash value calculation are pretty much random
+        return ~(hash(self.str) | hash(self.lang_code) * hash(self.gloss))
+    
+    def _tokenise(self):
         '''Splits a form into separate symbols.
         Detects polysymbollic segments such as affricates.'''
         segments = []
@@ -308,7 +250,7 @@ class Form:
         while i < len(self.str):
             cur_segment = self.str[i]
             next_segment = self.str[(i + 1) % len(self.str)]
-            if cur_segment + next_segment in polysymbols:
+            if cur_segment + next_segment in self.sp.polysymbols:
                 segments.append(cur_segment + next_segment)
                 i += 2
             else:
@@ -316,57 +258,54 @@ class Form:
                 i += 1
         return segments
     
-    def _to_features(self, features):
+    def _to_features(self):
         """Retrieves features for a given IPA symbol"""
         form_features = []
         for segment in self.segments:
-            segment_features = features.get(segment)
-            if segment_features is None:
-                return None
-            form_features.append(list(segment_features.items()))
+            segment_features = self.sp.features.get(segment)
+            form_features.append(segment_features)
         return form_features
     
-    def _to_symbols(self, form, features):
-        '''Returns symbols for a given set of features.
-        Is most porbably slow and not very efficient, as well as potentially unstable.'''
-        def process_segment(segment):
-            # leave only the features which are true for this segmetn
-            segment = [feature[0] for feature in segment if feature[1]]
-            # we need to sort it so that the order is not random
-            # it's quite a bit ugly
-            segment = tuple(sorted(segment))
-            return segment
-        def guess_symbol(segment):
-            ratios = {true_segments[true_segment]: difflib.SequenceMatcher(None,
-                                segment, 
-                                true_segment).ratio()
-                                for true_segment in true_segments}
-            return max(ratios, key=ratios.get)
-        # phew
-        flipped_features = {tuple(features[segment].items()): segment for segment in features}
-        true_segments = {process_segment(segment): flipped_features[segment] for segment in flipped_features}
-        # then let's process the features we are given
-        given_segments = [process_segment(segment) for segment in form]
+    def _to_symbols(self, form):
         symbols = []
-        for segment in given_segments:
-            if segment in true_segments:
-                symbols.append(true_segments[segment])
+        for segment in form:
+            if segment in self.sp.flipped_features:
+                symbols.append(self.sp.flipped_features[segment])
+            # if it's in there, get the closest one
             else:
-                symbols.append('(' + guess_symbol(segment) + ')')
+                sim_ratios = {symbol: SequenceMatcher(None,
+                                 self.sp.features[symbol], segment).ratio()
+                                      for symbol in self.sp.features}
+                closest_match = max(sim_ratios, key=sim_ratios.get)
+                symbols.append('({})'.format(closest_match))
         return symbols
+                
+    
+    @property
+    def structure(self):
+        struct = []
+        for feature in self.features:
+            if feature.cons:
+                struct.append('C')
+            else:
+                struct.append('V')
+        return struct
 
 class CognateSet:
     '''A cognate set is a set-like collection of Form objects'''
     
-    def __init__(self, forms):
-        if isinstance(forms, Form):
-            self.forms = {forms}
-        elif isinstance(forms, set):
-            for x in forms:
-                if not isinstance(x, Form):
-                    raise TypeError('Must be a list of Form objects')
-            else:
-                self.forms = forms
+    def __init__(self, forms=None):
+        if forms is None:
+            self.forms = set()
+        else:
+            if isinstance(forms, Form):
+                self.forms = {forms}
+            elif isinstance(forms, set):
+                for x in forms:
+                    if not isinstance(x, Form):
+                        raise TypeError('Must be a list of Form objects')
+                else:
+                    self.forms = forms
     
     def __iter__(self):
         return iter(self.forms)
@@ -397,10 +336,13 @@ class CognateSet:
                         'Need a form object, its string representation, or a language code')
     
     def __str__(self):
-        return str(self.forms)
+        return self.__repr__()
     
     def __repr__(self):
-        return str(self.forms)
+        if len(self.forms) == 0:
+            return 'CognateSet()'
+        else:
+            return 'CognateSet({})'.format(str(self.forms))
     
     def add(self, form):
         if isinstance(form, Form):
@@ -432,7 +374,7 @@ class CognateSet:
                 self.forms.remove(form)
             elif isinstance(form, str):
                 form_to_kill = self.get(form)
-                self.remove(form_to_kill)
+                self.forms.remove(form_to_kill)
             else:
                 raise BadFormQueryError('Need a Form object or str')
         
